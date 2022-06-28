7713af57ed2ae4d15b2861f6abded73aea4504c5
asamoah-fred.com <asamoahgodfred42@gmail.com>
2022-06-28 17:20:07 +0100

    added a welcome screen

---
+++ lib/welc.dart
@@ -0,0 +1,65 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:my_piano_tiles/home.dart';
+
+class Welcome extends StatelessWidget {
+  const Welcome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 40),
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/df.jpeg',
+                fit: BoxFit.contain,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'By: Asamoah Godfred',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(
+                      context, MaterialPageRoute(builder: (context) => Home()));
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue,
+                  ),
+                  child: Text(
+                    'Start',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
